48fd8ba2da2619ee4992f9f2fe7e101453ab4a11
Rafał Izdebski <rafal987.1@o2.pl>
2023-10-17 13:14:11 +0200

    feature/1-create-header #1 (#2)
    * feat: create html structure for header
    
    * chore: add header logo to assets
    
    * feat: create styles for header
    
    * feat: create fonts file
    
    * feat: create metrics file
    
    * feat: create typography file
    
    * fix: change media queries breakpoints
    
    * feat: create new scss variables
    
    * feat: add mixin for navLink
    
    * feat: import header to components
    
    * feat: create styles of page-wrapper and section
    
    * feat: create script for navbar items handle
    
    * feat: import new navbar feature

---
+++ src/js/features/navbar.js
@@ -0,0 +1,40 @@
+const navLinks = document.querySelectorAll('.nav__list-link');
+const sections = [...document.querySelectorAll('section')];
+const observerOption = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.6,
+};
+
+// set first section as initial active
+navLinks[0].classList.add('nav__list-link--active');
+
+navLinks.forEach((item) =>
+  item.addEventListener('click', (e) => toggleActiveClass(e))
+);
+
+const toggleActiveClass = (e) => {
+  navLinks.forEach((item) => {
+    item.classList.remove('nav__list-link--active');
+  });
+
+  e.target.classList.add('nav__list-link--active');
+};
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      const index = sections.indexOf(entry.target);
+      toggleActiveClass({ target: navLinks[index] });
+
+      // index 2 means 'producty' section, here we can handle start fetching products
+      if (index === 2) {
+        console.log('products section is visible we can fetch data');
+      }
+    }
+  });
+}, observerOption);
+
+sections.forEach((section) => {
+  observer.observe(section);
+});
